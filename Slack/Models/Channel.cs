172c434a3e7dd070430993204e71a07184386a49
NerkiTrzy <przemek.roguski@wp.pl>
2017-11-15 15:41:33 +0100

    created frontend to main page

---
+++ Slack/Models/Channel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slack.Models
+{
+    public class Channel
+    {
+        public int ID { get; set; }
+        public string OwnerID { get; set; }
+        public string Name { get; set; }
+        public ICollection<WorkspaceMembership> WorkspaceMemberships { get; set; }
+    }
+}
